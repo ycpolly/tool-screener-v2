1a81fbab8e910995993a6e1127cf50eef18f5d82
ycpolly <ycpolly@users.noreply.github.com>
2026-09-06 20:54:22 +0800

    chore(release): bump version to v0906.06 and update release notes

--- src/constants/ui-strings.js
+++ src/constants/ui-strings.js
@@ -7,7 +7,7 @@
 export const UI_STRINGS = {
   APP: {
     title: '豐盛幫手',
-    version: 'v0906.05',
+    version: 'v0906.06',
     prefixIntraday: '盤中 ',
     prefixClosed: '收盤 ',
     prefixPostMarket: '盤後 ',
